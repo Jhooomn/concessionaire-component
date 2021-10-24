ae1d5e5c4ca818d333f730c55e8bb9d4593cc8b5
Jhon Camilo Baron Berdugo <jhooomn@gmail.com>
2021-10-24 15:38:16 -0500

    management input fields were created

--- src/app/modules/components/vehicle/vehicle.component.ts
+++ src/app/modules/components/vehicle/vehicle.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vehicle',
   templateUrl: './vehicle.component.html',
-  styleUrls: ['./vehicle.component.css']
+  styleUrls: ['./vehicle.component.css'],
 })
 export class VehicleComponent implements OnInit {
+  managementsImage: String = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.managementsImage = 'https://bulma.io/images/placeholders/1280x960.png';
   }
-
 }
